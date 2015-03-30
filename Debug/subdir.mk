16c22242d06784df7bc74bfa7fd24b0f6d272272
dreimalb <mail.bjo@gmx.de>
2015-03-30 23:00:29 +0200

    Cleaned eclipse project files.

--- Debug/subdir.mk
+++ Debug/subdir.mk
@@ -4,26 +4,38 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
+../adc.c \
 ../irmp.c \
 ../irmpextlog.c \
-../main.c 
+../leds.c \
+../main.c \
+../pga.c \
+../spi.c 
 
 OBJS += \
+./adc.o \
 ./irmp.o \
 ./irmpextlog.o \
-./main.o 
+./leds.o \
+./main.o \
+./pga.o \
+./spi.o 
 
 C_DEPS += \
+./adc.d \
 ./irmp.d \
 ./irmpextlog.d \
-./main.d 
+./leds.d \
+./main.d \
+./pga.d \
+./spi.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 %.o: ../%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: AVR Compiler'
-	avr-gcc -Wall -g2 -gstabs -O2 -fpack-struct -fshort-enums -std=gnu99 -funsigned-char -funsigned-bitfields -mmcu=at90can128 -DF_CPU=16000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o"$@" "$<"
+	avr-gcc -Wall -g2 -gstabs -O2 -fpack-struct -fshort-enums -std=gnu99 -funsigned-char -funsigned-bitfields -mmcu=atmega168 -DF_CPU=20000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
